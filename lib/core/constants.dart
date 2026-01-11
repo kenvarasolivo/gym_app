140dbe8c75824f4bd8f127741b527de861c630a9
JamieEddie123 <james.edward.deutsch@gmail.com>
2026-01-11 20:37:17 +0100

    fixed constants

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xFF6200EE);
-const Color kBackgroundColor = Color(0xFF121212);
-const Color kCardColor = Color(0xFF1C1C1E);
+// ------------------- THEME & CONSTANTS -------------------
+const Color kPrimaryColor = Color(0xFFD0FD3E); // Lime Green
+const Color kBackgroundColor = Color(0xFF121212); // Deep Dark Background
+const Color kCardColor = Color(0xFF1C1C1E); // Slightly lighter for cards
 const double kPadding = 20.0;
